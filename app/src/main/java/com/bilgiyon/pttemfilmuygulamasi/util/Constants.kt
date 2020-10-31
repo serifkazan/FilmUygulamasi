dadff6849092cde681bbb2b0acdbd5acaa163246
serifkazan <serif_kazan@hotmail.com>
2020-10-31 23:18:53 +0300

    Update Constants.kt

--- app/src/main/java/com/bilgiyon/pttemfilmuygulamasi/util/Constants.kt
+++ app/src/main/java/com/bilgiyon/pttemfilmuygulamasi/util/Constants.kt
@@ -2,7 +2,7 @@ package com.bilgiyon.pttemfilmuygulamasi.util
 
 object Constants {
     const val BASE_URL = "https://api.themoviedb.org/3/"
-    const val API_KEY = "682375ea76078d73da428aff44da551f"
+    const val API_KEY = ""
     const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/"
     const val IMAGE_W92 = "w92"
     const val IMAGE_W154 = "w154"
